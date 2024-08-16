9ded4c61429e996fcd778c7b91b0c22b41e06ef2
prerna-b2 <prerna@bamboobox.ai>
2024-08-16 11:59:32 +0530

    Portfolio final

---
+++ src/components/Card.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Card = (props) => {
+  return (
+    <motion.div
+      initial={"hidden"}
+      whileInView={"visible"}
+      variants={{
+        visible: { opacity: 1 },
+        hidden: { opacity: 0 },
+      }}
+      class="max-w-xl bg-white rounded-lg border border-gray-200 shadow-lg dark:bg-gray-800 dark:border-gray-700 my-8"
+    >
+      <a href="hello">
+        <img
+          class="rounded-t-lg w-[574px] h-[414px]"
+          src={props.imagesrc}
+          alt=""
+        />
+      </a>
+      <div class="p-5">
+        <a href="helo">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {props.title}
+          </h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {props.summary}
+        </p>
+        {/* <a
+          href="hello"
+          class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300"
+        > */}
+          <div className="flex items-center">
+            <a
+              href={props.github_link}
+              className="mr-4 cursor-pointer hover:scale-125 flex justify-center items-center"
+              >
+              <img alt="" src={props.github_url} />
+            </a>
+            {/* Other card content here */}
+          </div>
+          {/* <svg
+            class="ml-2 -mr-1 w-4 h-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a> */}
+      </div>
+    </motion.div>
+  );
+};
+
+export default Card;
---
+++ src/views/Home.jsx
@@ -0,0 +1,98 @@
+import React, { useContext } from "react";
+import heroBg from "../assets/webdev.svg";
+import Typical from "react-typical";
+import { contactLinks } from "../constants";
+import { ThemeContext } from "../themeProvider";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
+import cloud from "../assets/cloudBg.png";
+import cloudDark from "../assets/cloudDark.png";
+import data from '../data.json';
+const Home = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <>
+      <div
+        style={
+          darkMode
+            ? { backgroundImage: `url('${cloud}')`, backgroundSize: "cover" }
+            : { backgroundImage: `url('${cloudDark}'`, backgroundSize: "cover" }
+        }
+      >
+        <main
+          className="mx-auto max-w-7xl px-4 sm:px-6 md:mt-0 lg:px-8 flex flex-col md:flex-row items-center justify-center md:justify-between h-screen"
+          id="/"
+        >
+          <div className="sm:text-center lg:text-left">
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <motion.span
+                className={darkMode ? "block text-black" : " text-white"}
+              >
+                Hi, I am {data.name}
+              </motion.span>
+              <span className="block text-blue-500 z-0 lg:inline">
+                <Typical
+                  steps={data.profiles.map((ele)=>ele)}
+                  loop={Infinity}
+                />
+              </span>
+            </h1>
+            <p
+              className={
+                darkMode
+                  ? "mt-3 text-base text-black sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+                  : "mt-3 text-base text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+              }
+            >
+              {data.Summary}
+            </p>
+            <div className="flex md:justify-start ">
+              {contactLinks.map((el) => (
+                <a
+                  href={el.link}
+                  className="mr-5 cursor-pointer mt-8 hover:scale-125"
+                >
+                  <img alt="" src={el.url} />
+                  {/* <p className="text-md mt-2 hover:hidden">{el.name}</p> */}
+                </a>
+              ))}
+            </div>
+            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+              <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
+              <a 
+                href={data.resumeLink} 
+                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
+                download
+              >
+                Resume
+              </a>
+
+{/* https://drive.google.com/file/d/1AMgqKkyFzrqzyKmPFbgu6mqbv0hprPLs/view?usp=drive_link */}
+              </div>
+            </div>
+          </div>
+          <motion.img
+            initial="hidden"
+            whileInView={"visible"}
+            variants={{
+              visible: {
+                y: 0,
+                opacity: 1,
+                transition: {
+                  type: "spring",
+                },
+              },
+              hidden: { opacity: 1, y: 80 },
+            }}
+            src={heroBg}
+            alt=""
+            className="md:w-3/6 hidden sm:block"
+          />
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Home;
